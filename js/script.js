2b4bfc72314b089f94b7e098a0b8c8e41fee5960
Adrian <klimasa98@gmail.com>
2023-06-24 08:50:16 -0400

    created btn and animation

--- js/script.js
+++ js/script.js
@@ -1,42 +1,7 @@
-const nav = document.querySelector('.nav')
-const navBtn = document.querySelector('.burger-btn')
-const allNavItems = document.querySelectorAll('.nav__item')
+const hamburger = document.querySelector('.hamburger')
 
-const handleNav = () => {
-  nav.classList.toggle('nav--active')
-
-  allNavItems.forEach(item => {
-    item.addEventListener('click', () => {
-      nav.classList.remove('nav--active')
-    })
-  })
-
-  handleNavItemsAnimation()
-  deleteAnimation()
+const handleNav = () =>{
+     hamburger.classList.toggle('hamburger--active')
 }
 
-const handleNavItemsAnimation = () => {
-  let delayTime = 0
-
-  allNavItems.forEach(item => {
-    item.classList.toggle('nav-items-animation')
-    item.style.animationDelay = '.' + delayTime + 's'
-    delayTime++
-  })
-}
-
-navBtn.addEventListener('click', handleNav)
-
-const deleteAnimation = () => {
-  allNavItems.forEach(item => {
-    item.addEventListener('click', () => {
-      allNavItems.forEach(el => {
-        el.classList.remove('nav-items-animation')
-      })
-    })
-  })
-}
-
-$(document).ready(function () {
-  $(window).scrollTop(0)
-})
+document.addEventListener('click', handleNav)
