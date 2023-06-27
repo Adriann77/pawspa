88601767152e09c87dddcaad39683d87859088de
Adrian <klimasa98@gmail.com>
2023-06-26 21:40:47 -0400

    change concept

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 const hamburger = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav')
 
 const handleNav = () =>{
      hamburger.classList.toggle('hamburger--active')
 }
 
+const navList = () =>{
+     nav.classList.toggle('nav--active')
+}
+
 hamburger.addEventListener('click', handleNav)
+hamburger.addEventListener('click', navList)
+
