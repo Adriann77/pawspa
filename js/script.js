17ce1c454a592c62f4cdfb6b5a5d5c6c4fa27b29
Adrian <klimasa98@gmail.com>
2023-06-28 23:11:18 -0400

    fixed nav problem and add js to nav

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const hamburger = document.querySelector('.hamburger')
 const nav = document.querySelector('.nav')
+const navLinks = document.querySelectorAll('.nav__item')
 
-const handleNav = () =>{
-     hamburger.classList.toggle('hamburger--active')
+const handleNav = () => {
+  hamburger.classList.toggle('hamburger--active')
 }
 
-const navList = () =>{
-     nav.classList.toggle('nav--active')
+const navList = () => {
+  nav.classList.toggle('nav--active')
 }
 
 hamburger.addEventListener('click', handleNav)
 hamburger.addEventListener('click', navList)
 
+navLinks.forEach(item =>
+  item.addEventListener('click', () => nav.classList.remove('nav--active'))
+)
