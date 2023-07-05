d2ac005d2ddee897f874d59dc603a2d5e9351926
Adrian <klimasa98@gmail.com>
2023-07-05 13:57:21 -0400

    changed concept of nav and add some js and backdropfilter to nav

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger')
 const nav = document.querySelector('.nav')
 const navLinks = document.querySelectorAll('.nav__link')
+const BgBtn = document.querySelector('.header__btn')
 
 const handleNav = () => {
   hamburger.classList.toggle('hamburger--active')
@@ -10,9 +11,18 @@ const navList = () => {
   nav.classList.toggle('nav--active')
 }
 
+const hideBtn = () =>{
+  BgBtn.classList.toggle('hide')
+}
+
+
+
 hamburger.addEventListener('click', handleNav)
 hamburger.addEventListener('click', navList)
+hamburger.addEventListener('click', hideBtn)
 
 navLinks.forEach(item =>
   item.addEventListener('click', () => nav.classList.remove('nav--active'))
 )
+
+
