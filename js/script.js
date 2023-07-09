0b5f517c90781234b71fd22314002f8d7698a60d
Adrian <klimasa98@gmail.com>
2023-07-09 19:03:14 -0400

    created adopt section

--- js/script.js
+++ js/script.js
@@ -3,42 +3,42 @@ const nav = document.querySelector('.nav')
 const navLinks = document.querySelectorAll('.nav__item')
 const BgBtn = document.querySelector('.header__btn')
 const footerYear = document.querySelector('.footer-year')
-const currYear =  new Date().getFullYear();
+const currYear = new Date().getFullYear()
 const navBackground = document.querySelector('.black-line')
+const adoptTitle = document.querySelector('.adopt-title')
 
-console.log(currYear);
+
+console.log(currYear)
 
 const handleNav = () => {
-  hamburger.classList.toggle('hamburger--active')
+	hamburger.classList.toggle('hamburger--active')
 }
 
 const navList = () => {
-  nav.classList.toggle('nav--active')
+	nav.classList.toggle('nav--active')
 }
 
 const hideBtn = () => {
-  BgBtn.classList.toggle('hide')
+	BgBtn.classList.toggle('hide')
+	
 }
 
 hamburger.addEventListener('click', handleNav)
 hamburger.addEventListener('click', navList)
 hamburger.addEventListener('click', hideBtn)
 
-navLinks.forEach(item =>
-  item.addEventListener('click', () => nav.classList.remove('nav--active'))
-)
 
-footerYear.textContent =  currYear
 
+navLinks.forEach(item => item.addEventListener('click', () => nav.classList.remove('nav--active')))
+
+footerYear.textContent = currYear
 
 const addShadow = () => {
-  if(window.scrollY > 105){
-    navBackground.classList.add('on')
-  }else{
-    navBackground.classList.remove('on')
-  }
-   
+	if (window.scrollY > 105) {
+		navBackground.classList.add('on')
+	} else {
+		navBackground.classList.remove('on')
+	}
 }
-
 
 window.addEventListener('scroll', addShadow)
